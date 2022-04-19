38978be654609d1160c1eea358fac1e894facc05
zhyea <zhangrui@zhyea.com>
2022-04-19 20:18:02 +0800

    添加IO操作工具类

---
+++ src/main/java/org/chobit/common/utils/IoKit.java
@@ -0,0 +1,64 @@
+package org.chobit.common.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * IO操作类
+ */
+public final class IoKit {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(IoKit.class);
+
+
+    /**
+     * 将数据保存到本地
+     *
+     * @param src        源数据
+     * @param targetPath 本地路径
+     */
+    public static void saveAs(byte[] src, String targetPath) {
+        try (FileOutputStream out = new FileOutputStream(targetPath)) {
+            out.write(src);
+            out.flush();
+        } catch (IOException e) {
+            logger.error("write to {} error.", targetPath, e);
+        }
+    }
+
+
+    /**
+     * 读取txt文本文件
+     *
+     * @param path 文件路径
+     * @return 读取到的文本
+     */
+    public static String readTxt(String path) {
+        try (
+                InputStream input = Files.newInputStream(Paths.get(path));
+                InputStreamReader read = new InputStreamReader(input);
+                BufferedReader bufferedReader = new BufferedReader(read)
+        ) {
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line);
+            }
+            return builder.toString();
+        } catch (IOException e) {
+            logger.error("read txt file failed. path:{}", path, e);
+        }
+        return "";
+    }
+
+
+    private IoKit() {
+        throw new UnsupportedOperationException("Private constructor, cannot be accessed.");
+    }
+
+}
